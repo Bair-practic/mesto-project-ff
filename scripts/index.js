913d86ed671ca2fd6ac772cb2e966c2e3c8eb626
bair <badmaiev2002@mail.ru>
2024-04-18 21:18:06 +0300

    ok

--- scripts/index.js
+++ scripts/index.js
@@ -7,27 +7,26 @@ const addButton = document.querySelector('.profile__add-button');
 let counter = 0;
 
 
-addButton.addEventListener('click', function (){
-    if (counterm <= 5){
+addButton.addEventListener('click', () => addCard());
+
+function addCard(){
+    if (counter <= 5){
         const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
         const deleteBtn = cardElement.querySelector('.card__delete-button');
 
         cardElement.querySelector('.card__title').textContent = initialCards[counter].name;
         cardElement.querySelector('.card__image').src = initialCards[counter].link;
-        deleteBtn.addEventListener('click', function(event){
-            deleteCard(event);
-        } )
+        deleteBtn.addEventListener('click', () => deleteCard(deleteBtn));
         cardList.append(cardElement);
         counter += 1;
     }
-})
-
- function deleteCard(evt){
-    console.log(evt.target);
-    console.log(evt);
-    cardList.remove(cardElement);
 }
 
+function deleteCard(deleteBtn) {
+    const listItem = deleteBtn.closest('.card');
+    listItem.remove()
+    counter -= 1;
+}
 
 
 // @todo: DOM узлы

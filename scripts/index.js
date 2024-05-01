18f60425eea611c978d202601d4bec923b2528a0
bair <badmaiev2002@mail.ru>
2024-05-01 22:29:32 +0300

    fixv3

--- scripts/index.js
+++ scripts/index.js
@@ -4,27 +4,26 @@ const cardList = document.querySelector('.places__list');
 const cardTemplate = document.querySelector('#card-template').content;
 const addButton = document.querySelector('.profile__add-button');
 
-
-
-initialCards.forEach(element => {
-    const card = createCard(element);
+initialCards.forEach(cardData => {
+    const card = createCard(cardData, deleteCard);
     cardList.append(card);
 
 });
-//Я не понял про какой хендлер речь идет. Перечитал теорию, там не описано, в задании тоже. Подскажите, пожалуйста как им пользоваться.
-function createCard(cardData){ 
+function createCard(cardData, deleteHandler){ 
     const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
     const deleteBtn = cardElement.querySelector('.card__delete-button');
     const cardImg = cardElement.querySelector('.card__image');
     cardElement.querySelector('.card__title').textContent = cardData.name;
     cardImg.src = cardData.link;
     cardImg.alt = cardData.name;
-    deleteBtn.addEventListener('click', () => deleteCard(deleteBtn));
+    deleteBtn.addEventListener('click', function(event) {
+        deleteHandler(event);
+    });
     return cardElement;
 }
 
-function deleteCard(deleteBtn) {
-    const listItem = deleteBtn.closest('.card');
+function deleteCard(event) {
+    const listItem = event.target.closest('.card');
     listItem.remove()
 }
 
